86869eb1d74749d0d04cf04b0297c0af91a15297
Achsah Abraham <achsahmp@gmail.com>
2018-08-03 16:56:41 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_numbers = ["1", "2", "3"]
   new_numbers.each do |number|
-    puts "This is number #{array[0]**2}"
+    puts "This is number #{array[0]}**2"
     puts "This is number #{array[1]**2}"
     puts "This is number #{array[2]**2}"
   end
